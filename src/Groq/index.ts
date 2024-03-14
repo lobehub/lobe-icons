dc1ca6a80404b7cdf472a15fbcdbdec1547b8203
Jiaqi Cai <45190310+jcdiv47@users.noreply.github.com>
2024-03-14 15:23:22 +0800

    🐛 fix: fix Groq miss combine (#13)
    Fixed Groq index.ts

--- src/Groq/index.ts
+++ src/Groq/index.ts
@@ -1,10 +1,12 @@
 import Avatar from './components/Avatar';
+import Combine from './components/Combine';
 import Mono from './components/Mono';
 import Text from './components/Text';
 import { COLOR_PRIMARY, TITLE } from './style';
 
 export type CompoundedIcon = typeof Mono & {
   Avatar: typeof Avatar;
+  Combine: typeof Combine;
   Text: typeof Text;
   colorPrimary: string;
   title: string;
@@ -13,6 +15,7 @@ export type CompoundedIcon = typeof Mono & {
 const Icons = Mono as CompoundedIcon;
 
 Icons.Text = Text;
+Icons.Combine = Combine;
 Icons.Avatar = Avatar;
 Icons.colorPrimary = COLOR_PRIMARY;
 Icons.title = TITLE;
