a4b9e07bbace1eb8973fa8f47a496a08f423b93c
canisminor1990 <i@canisminor.cc>
2024-11-24 18:43:03 +0800

    💄 style: Update og

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'dumi';
+import { SiteThemeConfig } from 'dumi-theme-lobehub';
+import { INavItem } from 'dumi/dist/client/theme-api/types';
 
 import { description, homepage, name } from './package.json';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const isWin = process.platform === 'win32';
 
-const themeConfig = {
+const nav: INavItem[] = [
+  { link: '/components/ai21', title: 'Icons' },
+  { link: '/features/model-icon', title: 'Components' },
+  { link: '/editor', title: 'SVG Editor' },
+  { link: 'https://ui.lobehub.com', mode: 'override', title: 'UI' },
+  { link: 'https://charts.lobehub.com', mode: 'override', title: 'Charts' },
+  { link: '/changelog', title: 'Changelog' },
+];
+
+const themeConfig: SiteThemeConfig = {
   actions: [
     {
       link: homepage,
@@ -18,29 +29,37 @@ const themeConfig = {
       type: 'primary',
     },
   ],
+  analytics: {
+    plausible: {
+      domain: 'icons.lobehub.com',
+      scriptBaseUrl: 'https://plausible.lobehub-inc.cn',
+    },
+  },
   apiHeader: {
     docUrl: `{github}/tree/master/src/{atomId}/index.md`,
-    match: ['/components'],
+    match: ['/components', '/features'],
     pkg: name,
     sourceUrl: `{github}/tree/master/src/{atomId}/index.tsx`,
   },
-  description: description,
-  footer: 'Made with 🤯 by LobeHub',
+  description,
   giscus: {
     category: 'Q&A',
     categoryId: 'DIC_kwDOLLYRIs4Cin-i',
     repo: 'lobehub/lobe-icons',
     repoId: 'R_kgDOLLYRIg',
   },
+  metadata: {
+    openGraph: {
+      image:
+        'https://repository-images.githubusercontent.com/750129442/59a8d16a-7ba1-4eda-b611-697d35cd04ab',
+    },
+  },
   name: 'Icons',
-  nav: [
-    { link: '/components/ai21', title: 'Icons' },
-    { link: '/features/model-icon', title: 'Components' },
-    { link: '/editor', title: 'SVG Editor' },
-    { link: 'https://ui.lobehub.com', mode: 'override', title: 'UI' },
-    { link: 'https://charts.lobehub.com', mode: 'override', title: 'Charts' },
-    { link: '/changelog', title: 'Changelog' },
-  ],
+  nav,
+  prefersColor: {
+    default: 'dark',
+    switch: false,
+  },
   socialLinks: {
     discord: 'https://discord.gg/AYFPHvv2jT',
     github: homepage,
