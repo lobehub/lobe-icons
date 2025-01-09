e21e393303872071052302eb9861a554ea84f12f
canisminor1990 <i@canisminor.cc>
2025-01-09 16:00:11 +0800

    ✨ feat: Export hooks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './features';
+export { useFillId, useFillIds } from './hooks/useFillId';
 export * from './icons';
 export { type IconToc, default as toc } from './toc';
 export type { IconType } from './types';
