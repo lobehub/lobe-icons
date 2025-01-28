828a74e8d84338072041a9543dcff2cca27051e5
Jason <ggbbddjm@gmail.com>
2025-01-28 10:14:12 +0800

    :sparkles: feat: update Novita AI logos and colors (#62)

--- src/Novita/index.ts
+++ src/Novita/index.ts
@@ -5,14 +5,13 @@ import Color from './components/Color';
 import Combine from './components/Combine';
 import Mono from './components/Mono';
 import Text from './components/Text';
-import { COLOR_GRADIENT, COLOR_PRIMARY, TITLE } from './style';
+import { COLOR_PRIMARY, TITLE } from './style';
 
 export type CompoundedIcon = typeof Mono & {
   Avatar: typeof Avatar;
   Color: typeof Color;
   Combine: typeof Combine;
   Text: typeof Text;
-  colorGradient: string;
   colorPrimary: string;
   title: string;
 };
@@ -23,7 +22,6 @@ Icons.Text = Text;
 Icons.Combine = Combine;
 Icons.Avatar = Avatar;
 Icons.colorPrimary = COLOR_PRIMARY;
-Icons.colorGradient = COLOR_GRADIENT;
 Icons.title = TITLE;
 
 export default Icons;
