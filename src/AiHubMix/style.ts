ce6782d561ce322357eaedfddda50d14e9e18394
jerlin <91647085+jerlinn@users.noreply.github.com>
2025-04-10 11:53:59 +0800

    ✨ feat: Add AihubMix (#93)
    * :bug: fix: remove unused background prop in AiHubMix Avatar component
    
    * 🐛 fix: 修复text暗色兼容、修复Avatar显示、移除packages目录下关联的静态资源

---
+++ src/AiHubMix/style.ts
@@ -0,0 +1,5 @@
+export const TITLE = 'AiHubMix';
+export const TEXT_MULTIPLE = 0.67; //文字高度比例
+export const SPACE_MULTIPLE = 0.3; //图文间距
+export const COLOR_PRIMARY = '#2563EB';
+export const COLOR_GRADIENT = 'linear-gradient(to bottom, #2563EB, #0049DF)';
