491d3fd68db8476e49eb135d5ba90caebdead866
BurnCloud.com <134291161+zuiyue-com@users.noreply.github.com>
2025-06-24 11:03:16 +0800

    💄 style: add BurnCloud model config (#125)
    * ✨ feat: add BurnCloud
    
    * 💄 style: add BurnCloud model config

--- src/features/providerEnum.ts
+++ src/features/providerEnum.ts
@@ -7,6 +7,7 @@ export enum ModelProvider {
   AzureAI = 'azureai',
   Baichuan = 'baichuan',
   Bedrock = 'bedrock',
+  BurnCloud = 'burncloud',
   Cloudflare = 'cloudflare',
   Cohere = 'cohere',
   DeepSeek = 'deepseek',
