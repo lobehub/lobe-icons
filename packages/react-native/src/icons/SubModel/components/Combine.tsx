6ce1b4dbec34ef07c6ba37c96082ed6e651e0f87
canisminor1990 <i@canisminor.cc>
2025-09-15 12:23:03 +0800

    ✨ feat: Add Vercel AI Gateway and SubModel

--- packages/react-native/src/icons/SubModel/components/Combine.tsx
+++ packages/react-native/src/icons/SubModel/components/Combine.tsx
@@ -1,26 +1,24 @@
-'use client';
+import React, { memo } from 'react';
 
-import { memo } from 'react';
-
-import IconCombine, { type IconCombineProps } from '@/features/IconCombine';
+import { RNIconCombine, type RNIconCombineProps } from '@/features';
 
 import { COMBINE_SPACE_MULTIPLE, COMBINE_TEXT_MULTIPLE, TITLE } from '../style';
 import Color from './Color';
 import Mono from './Mono';
 import Text from './Text';
 
-export interface CombineProps extends Omit<IconCombineProps, 'Icon' | 'Text'> {
+export interface CombineProps extends Omit<RNIconCombineProps, 'Icon' | 'Text'> {
   type?: 'color' | 'mono';
 }
 const Combine = memo<CombineProps>(({ type = 'mono', extraStyle, ...rest }) => {
   const Icon = type === 'color' ? Color : Mono;
 
   return (
-    <IconCombine
+    <RNIconCombine
       Icon={Icon}
       Text={Text}
       aria-label={TITLE}
-      extraStyle={{ fontWeight: 500, ...extraStyle }}
+      extraStyle={{ ...extraStyle }}
       spaceMultiple={COMBINE_SPACE_MULTIPLE}
       textMultiple={COMBINE_TEXT_MULTIPLE}
       {...rest}
--- src/SubModel/index.md
+++ src/SubModel/index.md
@@ -1,6 +1,6 @@
 ---
 nav: Components
-group: Model
+group: Provider
 title: SubModel
 atomId: SubModel
 description: https://submodel.ai
@@ -12,7 +12,12 @@ description: https://submodel.ai
 import { SubModel } from '@lobehub/icons';
 import { Flexbox } from 'react-layout-kit';
 
-export default () => <SubModel size={64} />;
+export default () => (
+  <Flexbox gap={16} horizontal>
+    <SubModel size={64} />
+    <SubModel.Color size={64} />
+  </Flexbox>
+);
 ```
 
 ## Text
@@ -32,6 +37,7 @@ import { Flexbox } from 'react-layout-kit';
 export default () => (
   <Flexbox gap={16} align={'flex-start'}>
     <SubModel.Combine size={64} />
+    <SubModel.Combine size={64} type={'color'} />
   </Flexbox>
 );
 ```
