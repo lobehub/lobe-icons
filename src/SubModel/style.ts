9f13ea0613a46f0514e427b921f20646397e798d
dd <1083962986@qq.com>
2025-09-15 12:04:52 +0800

    add SubModel.ai icon (#177)
    * add SubModel.ai icon
    
    * merge

---
+++ src/SubModel/style.ts
@@ -0,0 +1,9 @@
+export const TITLE = 'SubModel';
+export const COMBINE_TEXT_MULTIPLE = 0.6;
+export const COMBINE_SPACE_MULTIPLE = 0.1;
+export const COLOR_PRIMARY = '#1bc1fa';
+
+// Avatar constants
+export const AVATAR_BACKGROUND = '#fff';
+export const AVATAR_COLOR = '#fff';
+export const AVATAR_ICON_MULTIPLE = 0.75;
