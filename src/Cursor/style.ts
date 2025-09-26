61c8d5524b9c51497c43df30d1867e224294f53a
ericzakariasson <eric@anysphere.co>
2025-09-26 09:56:20 -0700

    🎨 chore: update cursor brand icon & colors

--- src/Cursor/style.ts
+++ src/Cursor/style.ts
@@ -1,7 +1,14 @@
 export const TITLE = 'Cursor';
-export const COMBINE_TEXT_MULTIPLE = 0.75;
+export const COMBINE_TEXT_MULTIPLE = 2.75;
 export const COMBINE_SPACE_MULTIPLE = 0.1;
 export const COLOR_PRIMARY = '#000';
+
+// Icon colors from Mono.tsx
+export const COLOR_BOTTOM = '#72716d';
+export const COLOR_LEFT = '#55544f';
+export const COLOR_RIGHT = '#43413c';
+export const COLOR_TOP_LIGHT = '#d6d5d2';
+export const COLOR_TOP_WHITE = '#fff';
 
 // Avatar constants
 export const AVATAR_BACKGROUND = COLOR_PRIMARY;
