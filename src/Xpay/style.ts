7b391e80bb52a63bbb3bf1ec2b5f469df4e78094
xPay <xpaysh@gmail.com>
2026-02-28 13:36:29 +0530

    ✨ feat: add xpay icon

---
+++ src/Xpay/style.ts
@@ -0,0 +1,9 @@
+export const TITLE = 'xpay';
+export const COMBINE_TEXT_MULTIPLE = 0.7;
+export const COMBINE_SPACE_MULTIPLE = 0.25;
+export const COLOR_PRIMARY = '#0F1C4D';
+
+// Avatar constants
+export const AVATAR_BACKGROUND = COLOR_PRIMARY;
+export const AVATAR_COLOR = '#00DC9C';
+export const AVATAR_ICON_MULTIPLE = 0.6;
