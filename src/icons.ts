ad63f3f8f9a4e687997ea0ce9f40abe7576f88c0
Adam Dalloul <adam.d.developer@gmail.com>
2026-06-12 02:40:30 -0700

    ✨ feat: add EmpirioLabs brand icon (Mono, Color, Text, Combine, Avatar)

--- src/icons.ts
+++ src/icons.ts
@@ -96,6 +96,7 @@ export { default as Doubao, type CompoundedIcon as DoubaoProps } from './Doubao'
 export { default as DreamMachine, type CompoundedIcon as DreamMachineProps } from './DreamMachine';
 export { default as ElevenLabs, type CompoundedIcon as ElevenLabsProps } from './ElevenLabs';
 export { default as ElevenX, type CompoundedIcon as ElevenXProps } from './ElevenX';
+export { default as EmpirioLabs, type CompoundedIcon as EmpirioLabsProps } from './EmpirioLabs';
 export { default as EssentialAI, type CompoundedIcon as EssentialAIProps } from './EssentialAI';
 export { default as Exa, type CompoundedIcon as ExaProps } from './Exa';
 export { default as Fal, type CompoundedIcon as FalProps } from './Fal';
