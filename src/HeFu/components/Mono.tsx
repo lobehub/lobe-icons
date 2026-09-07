1256a764caaebe58e49080239c733ba6014936fa
hefu-hk <mpoper@hefu.hk>
2026-09-07 17:43:59 +0800

    ✨ feat: add HeFu icon

---
+++ src/HeFu/components/Mono.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { memo } from 'react';
+
+import type { IconType } from '@/types';
+
+import { TITLE } from '../style';
+
+const Icon: IconType = memo(({ size = '1em', style, ...rest }) => {
+  return (
+    <svg
+      fill="currentColor"
+      fillRule="evenodd"
+      height={size}
+      style={{ flex: 'none', lineHeight: 1, ...style }}
+      viewBox="0 0 24 24"
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+      {...rest}
+    >
+      <title>{TITLE}</title>
+      <path d="M2.8 3.6h3.2v16.8H2.8Z M6 10.4h6v3.2H6Z M12 3.6h3.2v16.8H12Z M15.2 11.3h2.6v1.4h-2.6Z M19.3 5.6h1.4v12.8h-1.4Z M17.9 5.6a2.1 2.1 0 1 0 4.2 0a2.1 2.1 0 1 0-4.2 0Z M17.9 12a2.1 2.1 0 1 0 4.2 0a2.1 2.1 0 1 0-4.2 0Z M17.9 18.4a2.1 2.1 0 1 0 4.2 0a2.1 2.1 0 1 0-4.2 0Z" />
+    </svg>
+  );
+});
+
+export default Icon;
--- src/icons.ts
+++ src/icons.ts
@@ -138,6 +138,7 @@ export { default as Hailuo, type CompoundedIcon as HailuoProps } from './Hailuo'
 export { default as Haiper, type CompoundedIcon as HaiperProps } from './Haiper';
 export { default as HappyHorse, type CompoundedIcon as HappyHorseProps } from './HappyHorse';
 export { default as Hedra, type CompoundedIcon as HedraProps } from './Hedra';
+export { default as HeFu, type CompoundedIcon as HeFuProps } from './HeFu';
 export { default as HermesAgent, type CompoundedIcon as HermesAgentProps } from './HermesAgent';
 export { default as Higress, type CompoundedIcon as HigressProps } from './Higress';
 export { default as Huawei, type CompoundedIcon as HuaweiProps } from './Huawei';
