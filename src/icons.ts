acf451e2f3b9978e0617a2adf4596b06ec9aa648
AdairLi <AdairLi0309@outlook.com>
2026-09-21 05:31:04 +0100

    ✨ feat: add Dots Studio icon (#396)

--- src/icons.ts
+++ src/icons.ts
@@ -103,6 +103,7 @@ export { default as DigitalOcean, type CompoundedIcon as DigitalOceanProps } fro
 export { default as Doc2X, type CompoundedIcon as Doc2XProps } from './Doc2X';
 export { default as DocSearch, type CompoundedIcon as DocSearchProps } from './DocSearch';
 export { default as Dolphin, type CompoundedIcon as DolphinProps } from './Dolphin';
+export { default as DotsStudio, type CompoundedIcon as DotsStudioProps } from './DotsStudio';
 export { default as Doubao, type CompoundedIcon as DoubaoProps } from './Doubao';
 export { default as DreamMachine, type CompoundedIcon as DreamMachineProps } from './DreamMachine';
 export { default as ElevenLabs, type CompoundedIcon as ElevenLabsProps } from './ElevenLabs';
